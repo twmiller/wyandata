0f0f074e0431a25f75f1b0248cef9756e875c121
Thom Miller <twmiller@users.noreply.github.com>
2025-04-18 16:39:22 -0600

    Fixed missing code.

---
+++ api/urls.py
@@ -0,0 +1,9 @@
+# api/urls.py
+from django.urls import path
+from . import views
+
+# Define your API URL patterns here
+urlpatterns = [
+    # For now, we'll add a placeholder that we can test later
+    # path('test/', views.api_test, name='api_test'),
+]
---
+++ dashboard/urls.py
@@ -0,0 +1,8 @@
+# dashboard/urls.py
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    # Add views here when you're ready
+    # path('', views.index, name='index'),
+]
