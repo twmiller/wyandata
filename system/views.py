ad310bb06c24509c417747b174bc13225d45096e
Thom Miller <twmiller@users.noreply.github.com>
2025-04-18 18:15:42 -0600

    Added detailed system view

--- system/views.py
+++ system/views.py
@@ -23,3 +23,43 @@ def get_hosts(request):
         hosts_data.append(host_data)
     
     return Response(hosts_data)
+
+    @api_view(['GET'])
+    def get_host_details(request, host_id):
+        """Return detailed information about a specific host"""
+        try:
+            host = Host.objects.get(pk=host_id)
+        except Host.DoesNotExist:
+            return Response({'error': 'Host not found'}, status=404)
+        
+        # Get storage devices
+        storage_devices = []
+        for device in host.storage_devices.all():
+            storage_devices.append({
+                'id': str(device.id),
+                'name': device.name,
+                'device_type': device.device_type,
+                'total_bytes': device.total_bytes,
+            })
+        
+        # Get network interfaces
+        network_interfaces = []
+        for interface in host.network_interfaces.all():
+            network_interfaces.append({
+                'id': str(interface.id),
+                'name': interface.name,
+                'mac_address': interface.mac_address,
+                'ip_address': interface.ip_address,
+                'is_up': interface.is_up,
+            })
+        
+        # Build response
+        host_data = {
+            'id': str(host.id),
+            'hostname': host.hostname,
+            'system_type': host.system_type,
+            'ip_address': host.ip_address,
+            'cpu_model': host.cpu_model,
+            'cpu_cores': host.cpu_cores,
+            'ram_total': host.ram_total,
+            'gpu_model': host.
