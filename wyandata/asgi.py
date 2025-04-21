9d8879c868e7beb83643f4da46de3f966b00ef4c
Thomas Miller <twmiller@gmail.com>
2025-04-21 10:22:37 -0600

    Updated weather routing.

--- wyandata/asgi.py
+++ wyandata/asgi.py
@@ -1,30 +1,11 @@
 # wyandata/asgi.py
 import os
+from django.core.asgi import get_asgi_application
+from channels.routing import get_default_application
 import django
 
-# Set up Django first, before any other imports
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'wyandata.settings')
 django.setup()
 
-# Now import channels and other Django components
-from django.core.asgi import get_asgi_application
-from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.auth import AuthMiddlewareStack
-from channels.security.websocket import AllowedHostsOriginValidator
-
-# Only import app routing after Django is set up
-import solar.routing
-import system.routing  # Assuming you have system routing too
-
-# Create the ASGI application
-application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
-    "websocket": AllowedHostsOriginValidator(
-        AuthMiddlewareStack(
-            URLRouter(
-                solar.routing.websocket_urlpatterns +
-                system.routing.websocket_urlpatterns  # Include both routing patterns
-            )
-        )
-    ),
-})
+# Use the ASGI application with Channels routing
+application = get_default_application()
--- wyandata/routing.py
+++ wyandata/routing.py
@@ -2,18 +2,19 @@ from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
 
-# Import your app's routing modules
+# Import app routing modules
 import solar.routing
 import system.routing
-import weather.routing  # Add this line
+import weather.routing  # Make sure this import is present
 
 application = ProtocolTypeRouter({
     'websocket': AllowedHostsOriginValidator(
         AuthMiddlewareStack(
             URLRouter(
+                # Include all your app's websocket URL patterns
                 solar.routing.websocket_urlpatterns +
                 system.routing.websocket_urlpatterns +
-                weather.routing.websocket_urlpatterns  # Add this line
+                weather.routing.websocket_urlpatterns  # Make sure this is included
             )
         )
     ),
